a58cca644968ad8bc17fa51cc8bfb518c9206b2a
Matan Budimir <36965687+MatanBudimir@users.noreply.github.com>
2020-12-08 21:48:51 +0100

    Store sql files in separate folder

--- internal/app/eud_backup/start.go
+++ internal/app/eud_backup/start.go
@@ -2,15 +2,20 @@ package eud_backup
 
 import (
 	"eud_backup/internal/pkg/eud_backup/sql"
+	"fmt"
 	"log"
 	backup "eud_backup/internal/pkg/eud_backup"
+	"os"
+	"time"
 )
 
 func Start() {
 
 	databases := []string{
-		"eud_site", "eud_central",
+		"eud_site", "eud_central", "myvatsim", "central",
 	}
+
+	var folder = time.Now().UTC().Format("2020-12-12")
 
 	for _, name := range databases {
 		db, err := sql.Connect(name)
@@ -20,7 +25,29 @@ func Start() {
 			return
 		}
 
-		backup.Backup(db, name)
+		if _, err := os.Stat("dumps/" + folder); os.IsNotExist(err) {
+
+			err := createDirctory(fmt.Sprintf("dumps/%s", folder), 0775)
+
+			if err != nil {
+				log.Println(err)
+
+				continue
+			}
+
+		}
+
+		backup.Backup(db, name, folder)
 	}
 
+}
+
+func createDirctory(name string, perms os.FileMode) error {
+	err := os.Mkdir(name, perms)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/pkg/eud_backup/backup.go
+++ internal/pkg/eud_backup/backup.go
@@ -11,10 +11,10 @@ import (
 // Represents the dump directory
 var dumpDir string = "dumps"
 
-func Backup(db *sql.DB, dbname string) {
+func Backup(db *sql.DB, dbname , folder string) {
 	var fileName = name(dbname)
 
-	dumper, err := mysqldump.Register(db, dumpDir, fileName)
+	dumper, err := mysqldump.Register(db, dumpDir + "/" + folder, fileName)
 
 	if err != nil {
 		log.Println(err)
@@ -34,6 +34,5 @@ func Backup(db *sql.DB, dbname string) {
 }
 
 func name(dbname string) string {
-	now := time.Now()
-	return fmt.Sprintf("%s_%d_%d_%d", dbname, now.Year(), now.Month(), now.Day())
+	return fmt.Sprintf("%s-%v", dbname, time.Now().UnixNano())
 }
