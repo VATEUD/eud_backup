314f9141c2bbfaa65c9c6cb8993b65e93e158aff
Matan Budimir <matanbudimir@gmail.com>
2020-12-11 16:52:28 +0100

    Using db name as the file name

--- internal/pkg/eud_backup/backup.go
+++ internal/pkg/eud_backup/backup.go
@@ -2,19 +2,16 @@ package eud_backup
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/JamesStewy/go-mysqldump"
 	"log"
-	"time"
 )
 
 // Represents the dump directory
 var dumpDir string = "dumps"
 
 func Backup(db *sql.DB, dbname, folder string) {
-	var fileName = name(dbname)
 
-	dumper, err := mysqldump.Register(db, dumpDir+"/"+folder, fileName)
+	dumper, err := mysqldump.Register(db, dumpDir+"/"+folder, dbname)
 
 	if err != nil {
 		log.Println(err)
@@ -31,8 +28,4 @@ func Backup(db *sql.DB, dbname, folder string) {
 	log.Printf("File saved (%s)", result)
 
 	dumper.Close()
-}
-
-func name(dbname string) string {
-	return fmt.Sprintf("%s-%v", dbname, time.Now().UnixNano())
 }
